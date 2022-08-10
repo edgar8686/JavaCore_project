88e441682b20692bd839b3c2aaaa55fc86a2b4bb
Edgar <edgar.nurmagomedov@mail.ru>
2022-08-10 22:17:23 +0500

    Homework 3

---
+++ src/Lesson3/Apple.java
@@ -0,0 +1,8 @@
+package Lesson3;
+
+public class Apple extends Fruit {
+    @Override
+    float getWeight() {
+        return 1.6f;
+    }
+}
---
+++ src/Lesson3/Orange.java
@@ -0,0 +1,8 @@
+package Lesson3;
+
+public class Orange extends Fruit {
+    @Override
+    float getWeight() {
+        return 1.3f;
+    }
+}
